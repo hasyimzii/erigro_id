31de75e776aab09159cedcb5af17296abd8d0c35
joo <arihasyim.zii@gmail.com>
2021-06-12 22:27:03 +0700

    First routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,4 +1,15 @@
 <?php 
 class App {
-    
+    public function __construct() {
+        $url = $this->parseURL();
+    }
+
+    public function parseURL() {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
 }
