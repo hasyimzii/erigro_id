48467a1bf3d694d1721bab4aa5ec00169b3b3ce2
joo <arihasyim.zii@gmail.com>
2021-06-14 02:47:03 +0700

    first controller

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,36 @@
 <?php 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct() {
         $url = $this->parseURL();
+        
+        // controller
+        if(file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // method
+        if(isset($url[1])) {
+            if(method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // params
+        if(!empty($url)) {
+            $this->params = array_values($url);
+        }
+
+        // run controller
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL() {
