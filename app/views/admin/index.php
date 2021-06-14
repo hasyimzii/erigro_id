d1be4dbabcb33e59d450d7846a7d9aadbb3f7354
achmadgalehindra <achmadgalehindra@gmail.com>
2021-06-14 23:12:31 +0700

    Admin

---
+++ app/views/admin/index.php
@@ -0,0 +1,129 @@
+<?php 
+    include '../app/views/main/app.php';
+?>
+
+<?= template_header('Home') ?>
+<?= template_navbar() ?>
+<?= template_sidebar() ?>
+<div class="main-container">
+<!-- Simple Datatable End -->
+<!-- multiple select row Datatable start -->
+<div class="card-box mb-30">
+    <div class="pd-20">
+        <h4 class="text-blue h4">Data Informasi Bibit Erigro.id</h4>
+    </div>
+    <div class="pb-20">
+        <table class="data-table table hover multiple-select-row nowrap">
+            <thead>
+                <tr>
+                    <th class="table-plus datatable-nosort">Name</th>
+                    <th>Age</th>
+                    <th>Office</th>
+                    <th>Address</th>
+                    <th>Start Date</th>
+                    <th>Salart</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="table-plus">Gloria F. Mead</td>
+                    <td>25</td>
+                    <td>Sagittarius</td>
+                    <td>2829 Trainer Avenue Peoria, IL 61602 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Gemini</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>20</td>
+                    <td>Gemini</td>
+                    <td>2829 Trainer Avenue Peoria, IL 61602 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Sagittarius</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>25</td>
+                    <td>Gemini</td>
+                    <td>2829 Trainer Avenue Peoria, IL 61602 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>20</td>
+                    <td>Sagittarius</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>18</td>
+                    <td>Gemini</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Sagittarius</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Sagittarius</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Gemini</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Gemini</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+                <tr>
+                    <td class="table-plus">Andrea J. Cagle</td>
+                    <td>30</td>
+                    <td>Gemini</td>
+                    <td>1280 Prospect Valley Road Long Beach, CA 90802 </td>
+                    <td>29-03-2018</td>
+                    <td>$162,700</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+<?= template_footer() ?>
--- app/views/main/app.php
+++ app/views/main/app.php
@@ -182,7 +182,7 @@ function template_navbar() {
                         <span class="user-icon">
                             <img src="vendors/images/photo1.jpg" alt="">
                         </span>
-                        <span class="user-name">Ross C. Lopez</span>
+                        <span class="user-name">Admin</span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
                         <a class="dropdown-item" href="profile.html"><i class="dw dw-user1"></i> Profile</a>
@@ -214,161 +214,37 @@ function template_sidebar() {
             <div class="sidebar-menu">
                 <ul id="accordion-menu">
                     <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
+                        <a href="javascript:;" class="dropdown-toggle no-arrow">
                             <span class="micon dw dw-house-1"></span><span class="mtext">Home</span>
                         </a>
-                        <ul class="submenu">
-                            <li><a href="index.html">Dashboard style 1</a></li>
-                            <li><a href="index2.html">Dashboard style 2</a></li>
-                        </ul>
                     </li>
                     <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-edit2"></span><span class="mtext">Forms</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="form-basic.html">Form Basic</a></li>
-                            <li><a href="advanced-components.html">Advanced Components</a></li>
-                            <li><a href="form-wizard.html">Form Wizard</a></li>
-                            <li><a href="html5-editor.html">HTML5 Editor</a></li>
-                            <li><a href="form-pickers.html">Form Pickers</a></li>
-                            <li><a href="image-cropper.html">Image Cropper</a></li>
-                            <li><a href="image-dropzone.html">Image Dropzone</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-library"></span><span class="mtext">Tables</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="basic-table.html">Basic Tables</a></li>
-                            <li><a href="datatable.html">DataTables</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="calendar.html" class="dropdown-toggle no-arrow">
-                            <span class="micon dw dw-calendar1"></span><span class="mtext">Calendar</span>
+                        <a href="javascript:;" class="dropdown-toggle no-arrow">
+                            <span class="micon dw dw-library"></span><span class="mtext">Informasi Bibit</span>
                         </a>
                     </li>
                     <li class="dropdown">
                         <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-apartment"></span><span class="mtext"> UI Elements </span>
+                            <span class="micon dw dw-apartment"></span><span class="mtext">Konfirmasi Pengajuan</span>
                         </a>
                         <ul class="submenu">
-                            <li><a href="ui-buttons.html">Buttons</a></li>
-                            <li><a href="ui-cards.html">Cards</a></li>
-                            <li><a href="ui-cards-hover.html">Cards Hover</a></li>
-                            <li><a href="ui-modals.html">Modals</a></li>
-                            <li><a href="ui-tabs.html">Tabs</a></li>
-                            <li><a href="ui-tooltip-popover.html">Tooltip &amp; Popover</a></li>
-                            <li><a href="ui-sweet-alert.html">Sweet Alert</a></li>
-                            <li><a href="ui-notification.html">Notification</a></li>
-                            <li><a href="ui-timeline.html">Timeline</a></li>
-                            <li><a href="ui-progressbar.html">Progressbar</a></li>
-                            <li><a href="ui-typography.html">Typography</a></li>
-                            <li><a href="ui-list-group.html">List group</a></li>
-                            <li><a href="ui-range-slider.html">Range slider</a></li>
-                            <li><a href="ui-carousel.html">Carousel</a></li>
+                            <li><a href="ui-buttons.html">Pengajuan Perorangan</a></li>
+                            <li><a href="ui-cards.html">Pengajuan Kelompok</a></li>
+                            <li><a href="ui-cards-hover.html">Pengajuan Institusi</a></li>
                         </ul>
                     </li>
                     <li class="dropdown">
                         <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-paint-brush"></span><span class="mtext">Icons</span>
+                            <span class="micon dw dw-paint-brush"></span><span class="mtext">Konfirmasi Konseling</span>
                         </a>
                         <ul class="submenu">
-                            <li><a href="font-awesome.html">FontAwesome Icons</a></li>
-                            <li><a href="foundation.html">Foundation Icons</a></li>
-                            <li><a href="ionicons.html">Ionicons Icons</a></li>
-                            <li><a href="themify.html">Themify Icons</a></li>
-                            <li><a href="custom-icon.html">Custom Icons</a></li>
+                            <li><a href="font-awesome.html">Data Konseling</a></li>
+                            <li><a href="foundation.html">Hasil Konseling</a></li>
                         </ul>
                     </li>
                     <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
+                        <a href="javascript:;" class="dropdown-toggle no-arrow">
                             <span class="micon dw dw-analytics-21"></span><span class="mtext">Charts</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="highchart.html">Highchart</a></li>
-                            <li><a href="knob-chart.html">jQuery Knob</a></li>
-                            <li><a href="jvectormap.html">jvectormap</a></li>
-                            <li><a href="apexcharts.html">Apexcharts</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-right-arrow1"></span><span class="mtext">Additional Pages</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="video-player.html">Video Player</a></li>
-                            <li><a href="login.html">Login</a></li>
-                            <li><a href="forgot-password.html">Forgot Password</a></li>
-                            <li><a href="reset-password.html">Reset Password</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-browser2"></span><span class="mtext">Error Pages</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="400.html">400</a></li>
-                            <li><a href="403.html">403</a></li>
-                            <li><a href="404.html">404</a></li>
-                            <li><a href="500.html">500</a></li>
-                            <li><a href="503.html">503</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-copy"></span><span class="mtext">Extra Pages</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="blank.html">Blank</a></li>
-                            <li><a href="contact-directory.html">Contact Directory</a></li>
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="blog-detail.html">Blog Detail</a></li>
-                            <li><a href="product.html">Product</a></li>
-                            <li><a href="product-detail.html">Product Detail</a></li>
-                            <li><a href="faq.html">FAQ</a></li>
-                            <li><a href="profile.html">Profile</a></li>
-                            <li><a href="gallery.html">Gallery</a></li>
-                            <li><a href="pricing-table.html">Pricing Tables</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-list3"></span><span class="mtext">Multi Level Menu</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="javascript:;">Level 1</a></li>
-                            <li><a href="javascript:;">Level 1</a></li>
-                            <li><a href="javascript:;">Level 1</a></li>
-                            <li class="dropdown">
-                                <a href="javascript:;" class="dropdown-toggle">
-                                    <span class="micon fa fa-plug"></span><span class="mtext">Level 2</span>
-                                </a>
-                                <ul class="submenu child">
-                                    <li><a href="javascript:;">Level 2</a></li>
-                                    <li><a href="javascript:;">Level 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="javascript:;">Level 1</a></li>
-                            <li><a href="javascript:;">Level 1</a></li>
-                            <li><a href="javascript:;">Level 1</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="sitemap.html" class="dropdown-toggle no-arrow">
-                            <span class="micon dw dw-diagram"></span><span class="mtext">Sitemap</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="chat.html" class="dropdown-toggle no-arrow">
-                            <span class="micon dw dw-chat3"></span><span class="mtext">Chat</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="invoice.html" class="dropdown-toggle no-arrow">
-                            <span class="micon dw dw-invoice"></span><span class="mtext">Invoice</span>
                         </a>
                     </li>
                     <li>
@@ -378,20 +254,9 @@ function template_sidebar() {
                         <div class="sidebar-small-cap">Extra</div>
                     </li>
                     <li>
-                        <a href="javascript:;" class="dropdown-toggle">
-                            <span class="micon dw dw-edit-2"></span><span class="mtext">Documentation</span>
-                        </a>
-                        <ul class="submenu">
-                            <li><a href="introduction.html">Introduction</a></li>
-                            <li><a href="getting-started.html">Getting Started</a></li>
-                            <li><a href="color-settings.html">Color Settings</a></li>
-                            <li><a href="third-party-plugins.html">Third Party Plugins</a></li>
-                        </ul>
-                    </li>
-                    <li>
                         <a href="https://dropways.github.io/deskapp-free-single-page-website-template/" target="_blank" class="dropdown-toggle no-arrow">
                             <span class="micon dw dw-paper-plane1"></span>
-                            <span class="mtext">Landing Page <img src="vendors/images/coming-soon.png" alt="" width="25"></span>
+                            <span class="mtext">Users Website <img src="vendors/images/coming-soon.png" alt="" width="25"></span>
                         </a>
                     </li>
                 </ul>
@@ -410,12 +275,20 @@ function template_footer() {
         <script src="vendors/scripts/script.min.js"></script>
         <script src="vendors/scripts/process.js"></script>
         <script src="vendors/scripts/layout-settings.js"></script>
-        <script src="src/plugins/apexcharts/apexcharts.min.js"></script>
         <script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
         <script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
         <script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
         <script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
-        <script src="vendors/scripts/dashboard.js"></script>
+        <!-- buttons for Export datatable -->
+        <script src="src/plugins/datatables/js/dataTables.buttons.min.js"></script>
+        <script src="src/plugins/datatables/js/buttons.bootstrap4.min.js"></script>
+        <script src="src/plugins/datatables/js/buttons.print.min.js"></script>
+        <script src="src/plugins/datatables/js/buttons.html5.min.js"></script>
+        <script src="src/plugins/datatables/js/buttons.flash.min.js"></script>
+        <script src="src/plugins/datatables/js/pdfmake.min.js"></script>
+        <script src="src/plugins/datatables/js/vfs_fonts.js"></script>
+        <!-- Datatable Setting js -->
+        <script src="vendors/scripts/datatable-setting.js"></script></body>
     </body>
     </html>
     EOT;
